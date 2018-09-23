c329304ea3d01cf513acb7f97a57dc4412f45fc8
Omori Takayuki <takayuki.o@OmorinoMBP.elecom>
2018-09-23 22:59:14 +0900

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '2695622e3e66c930f2534e5d21cb54a0cb302b154bc77b70988859facefb5046a2373d71779560578ceb8178669669767c02f3b75f2149cee3ce60c3aecdd3cb'
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
+
+  end
+
+resources :users, only: [:show, :edit]
+
+resources :users, only: [:show, :edit, :update]
+
+
+  root 'post_images#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
